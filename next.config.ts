f44b368cc97c6031bec66a8573c4c43885c24c16
yuurisha <yourisha.sofea@gmail.com>
2026-01-10 01:14:11 +0800

    fix: Resolve merge conflict in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,13 +6,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-<<<<<<< HEAD
-=======
-
   typescript: {
     ignoreBuildErrors: true,
   },
->>>>>>> a681b1ecf5e2d54c51ae9172af9e85a0b655a9c7
+
 };
 
 export default nextConfig;
